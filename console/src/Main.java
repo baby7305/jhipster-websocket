2d13a287fc7a82e6955ef593f3f78d645cc1f646
baby7305 <songhaibei7305@163.com>
2019-03-26 00:10:09 +0800

    Initial application generated by JHipster-5.8.2

---
+++ console/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+
+    public static void main(String[] args) {
+        if (Float.valueOf("9601150") == 9601152) {
+            System.out.println(Float.valueOf("9601150"));
+            System.out.println("979601160的float类型字符串等于979601152");
+        } else {
+            System.out.println("float不等于");
+        }
+//
+//
+//        if (Integer.valueOf("979601160") == 979601152) {
+//            System.out.println("979601160的integer类型字符串等于979601152");
+//        }
+//        System.out.println("integer不等于");
+//
+//        if (Double.valueOf("979601160") == 979601152) {
+//            System.out.println("979601160的double类型字符串等于979601152");
+//        }
+//        System.out.println("double不等于");
+
+    }
+}
